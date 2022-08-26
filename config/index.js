7952e4813b94f6eaef11641caeea25c7e18bef15
wangxinleo <wangxin.leo@outlook.com>
2022-08-26 14:15:37 +0800

    🎁feat(女朋友每日提醒): 第一个可用版本

---
+++ config/index.js
@@ -0,0 +1,38 @@
+export const config = {
+    // 公众号配置
+    // 公众号appId
+    appId: "",
+    // 公众号appSecret
+    appSecret: "",
+    // 模板消息id
+    templateId: "",
+    // 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如["wx1", "wx2"]
+    user: [],
+     
+    // 信息配置
+    // 所在省份
+    province: "广东",
+    // 所在城市
+    city: "惠州",
+    // 生日，修改名字为对应需要显示的名字，如果生日为农历， type为 r
+    birthdays: [
+      {"name": "老婆", "year": "1993", "date": "12-27", "type": 'new'},
+      {"name": "家公", "year": "1993", "date": "08-09", "type": 'new'},
+    ],
+    // 在一起的日子，格式同上
+    loveDate: "2015-05-01",
+    // 结婚纪念日
+    marryDate: "2020-01-04"
+    }
+
+// {{date.DATA}}  
+// 城市：{{city.DATA}}  
+// 天气：{{weather.DATA}}  
+// 最低气温: {{min_temperature.DATA}}  
+// 最高气温: {{max_temperature.DATA}}  
+// 今天是我们恋爱的第{{love_day.DATA}}天
+// 今天是我们结婚的第{{marry_day.DATA}}天
+// {{birthday_message.DATA}}
+
+// {{note_en.DATA}}  
+// {{note_ch.DATA}}
--- main.js
+++ main.js
@@ -120,9 +120,9 @@ const getBirthdayMessage = () => {
         const nextBir = dayjs(dayjs().format('YYYY') + '-' + birthday.date).diff(dayjs(), 'day')
         
         if (nextBir === 0) {
-            birthdayMessage = `今天是 ${birthday.name} 生日哦，祝 ${birthday.name} 生日快乐！`
+            birthdayMessage = `今天是 ${birthday.name} 生日哦，祝${birthday.name}生日快乐！`
         } else if (nextBir > 0 ) {
-            birthdayMessage = `距离 ${birthday.name} 的生日还有 ${nextBir} 天`
+            birthdayMessage = `距离 ${birthday.name} 的生日还有${nextBir}天`
         }
         // 存储数据
         birthday['message'] = birthdayMessage
