71348e5106206c37ec489dae47b8e79abdf948bc
ttaohe <110707902+ttaohe@users.noreply.github.com>
2022-08-27 16:41:56 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -7,7 +7,7 @@ export const config = {
     // 模板消息id
     templateId: "QnDgAXjBbmwWMrmtOq7g5afWdnodOMVrhS-Mi3kW5J0",
     // 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如["wx1", "wx2"]
-    user: ["潘多拉魔盒",],
+    user: ["ooeIf5sLXk8QDYDplJgJeaOjaHrI",],
      
     // 信息配置
     // 所在省份
