f4bed6166014a48398e7284f80f57e3bb0adce7b
Lordon <xunjtech@gmail.com>
2022-08-28 09:20:19 +0800

    master

--- config/index.js
+++ config/index.js
@@ -13,7 +13,7 @@ export const config = {
     // 所在省份
     province: "北京",
     // 所在城市
-    city: "石景山",
+    city: "北京",
     // 生日，修改名字为对应需要显示的名字，如果生日为农历， type为 r
     birthdays: [
       {"name": "老婆", "year": "1995", "date": "10-04", "type": 'new'},
