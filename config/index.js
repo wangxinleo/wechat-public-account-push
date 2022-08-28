6fd7b10a762ee24d42e168054b43c18fe1583cb7
Lordon <xunjtech@gmail.com>
2022-08-28 09:23:02 +0800

    change test id and psd

--- config/index.js
+++ config/index.js
@@ -1,13 +1,15 @@
 export const config = {
     // 公众号配置
     // 公众号appId
-    appId: "wxe01fac26b8d34c0b",
+    // appId: "wxe01fac26b8d34c0b",
+    appId: "wx1a1ad6c8238ced4d", // for test
     // 公众号appSecret
-    appSecret: "65d7f90aff8cd8a79c2812432cf5a723",
+    // appSecret: "65d7f90aff8cd8a79c2812432cf5a723",
+    appSecret: "c4a758f7458ba2a0905cdff7d228ebac", // for test
     // 模板消息id
     templateId: "MJ7nYyy4Bhtm4EldImvn8PCexk1pwulyffP_TMM0lXs",
     // 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如["wx1", "wx2"]
-    user: ["oi-Mf5gQn-Hqg76ydp3RV39-1FB4"],
+    user: ["oi-Mf5gQn-Hqg76ydp3RV39-1FB4"], // lordon
      
     // 信息配置
     // 所在省份
@@ -16,13 +18,13 @@ export const config = {
     city: "北京",
     // 生日，修改名字为对应需要显示的名字，如果生日为农历， type为 r
     birthdays: [
-      {"name": "老婆", "year": "1995", "date": "10-04", "type": 'new'},
-      {"name": "家公", "year": "1997", "date": "08-09", "type": 'new'},
+      {"name": "小兔子", "year": "1995", "date": "10-04", "type": 'new'},
+      {"name": "大猪蹄子", "year": "1997", "date": "02-02", "type": 'new'},
     ],
     // 在一起的日子，格式同上
     loveDate: "2019-11-31",
     // 结婚纪念日
-    marryDate: "2022-06-09"
+    marryDate: "2022-08-09"
     }
 
 // {{date.DATA}}  
