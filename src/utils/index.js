10c54028e2818e1954ca7973c956da739d77bcdf
wangxinleo <wangxin.leo@outlook.com>
2022-08-28 16:59:37 +0800

    feat(wechat-public-account-push): 新增好文节选
    - 修改了常量的格式，更符合规范了！
    
    - 应要求加入了文学类的出参

--- src/utils/index.js
+++ src/utils/index.js
@@ -4,11 +4,31 @@
  * @returns 
  */
 export const toLowerLine =  (str) => {
-    var temp = str.replace(/[A-Z]/g, function (match) { 
-     return "_" + match.toLowerCase();
-      });
-      if(temp.slice(0,1) === '_'){ //如果首字母是大写，执行replace时会多一个_，这里需要去掉
-       temp = temp.slice(1);
-      }
-    return temp;
-   };
+  var temp = str.replace(/[A-Z]/g, function (match) { 
+    return "_" + match.toLowerCase();
+    });
+    if(temp.slice(0,1) === '_'){ //如果首字母是大写，执行replace时会多一个_，这里需要去掉
+      temp = temp.slice(1);
+    }
+  return temp;
+};
+
+
+/**
+ * 获取随机颜色
+ * @returns 
+ */
+export const getColor = () => {
+  return `#${Math.floor(Math.random() * 0xffffff).toString(16).padEnd(6, "0")}`
+}
+
+
+/**
+ * 生成一个从min 到 max 的随机数
+ * @param {*} min 
+ * @param {*} max 
+ * @returns 
+ */
+export const randomNum = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1)) + min
+}
