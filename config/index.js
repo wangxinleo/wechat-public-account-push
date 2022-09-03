cba458a8d61fe4129cebadd29ee3718c2e095116
wangxinleo <wangxin.leo@outlook.com>
2022-09-03 23:34:36 +0800

    feat(wechat-public-account-push): 支持多个城市

--- config/index.js
+++ config/index.js
@@ -31,7 +31,11 @@ export const config = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "ohxOO6VskYv0Kuf-ywIZVRMpuXhk",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "zjVglIuCXo4CsraXWEio1iP7sZJ7IQEES9lDsgc4yOU",
+      useTemplateId: "Wue80GrtY1nAia4tam-FDxZF9WNx9pwcBc0LMpANnu8",
+      // 所在省份
+      province: "广东",
+      // 所在城市
+      city: "肇庆",
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
       openUrl: "https://wangxinleo.cn"
     }, 
@@ -39,18 +43,24 @@ export const config = {
       name: "老婆1", 
       id: "ohxOO6VskYv0Kuf-ywIZVRMpuXhk",
       useTemplateId: "Wue80GrtY1nAia4tam-FDxZF9WNx9pwcBc0LMpANnu8",
+      province: "广东",
+      city: "河源",
       openUrl: "https://baidu.com"
     }, 
     {
       name: "老婆2",
       id: "ohxOO6VskYv0Kuf-ywIZVRMpuXhk",
-      useTemplateId: "zjVglIuCXo4CsraXWEio1iP7sZJ7IQEES9lDsgc4yOU",
+      useTemplateId: "Wue80GrtY1nAia4tam-FDxZF9WNx9pwcBc0LMpANnu8",
+      province: "广东",
+      city: "广州",
       openUrl: "https://weibo.com"
     }, 
     {
       name: "老婆3", 
       id: "ohxOO6VskYv0Kuf-ywIZVRMpuXhk", 
       useTemplateId: "Wue80GrtY1nAia4tam-FDxZF9WNx9pwcBc0LMpANnu8",
+      province: "",
+      city: "",
       openUrl: "https://wangxinleo.cn"
     }, 
   ],
