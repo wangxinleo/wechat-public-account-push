1c8001752a69c04b2902d4da083ac55547ae3d37
shuangxunian <shuangxunian@gmail.com>
2022-09-04 15:17:54 +0800

    fix

--- config/index.js
+++ config/index.js
@@ -31,7 +31,7 @@ export const config = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "os5lv55_9CrmT2wRucTIy85kDnC8",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "	509pVwhkOhSDkFWnN3E0NL_udjwK9-6LVPtheQn-8fI",
+      useTemplateId: "509pVwhkOhSDkFWnN3E0NL_udjwK9-6LVPtheQn-8fI",
       // 所在省份
       province: "广东",
       // 所在城市
