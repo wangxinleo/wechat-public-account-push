cd7826d0a005d83ca148086648a552e8a7c39975
wangxin.leo <wangxin.leo@outlook.com>
2022-09-08 07:26:54 +0800

    Update main.js

--- main.js
+++ main.js
@@ -110,7 +110,7 @@ const mainForTest = async () => {
     // 测试UTC时间在actions里的区别
     console.log('dayjs', dayjs())
     console.log('selfDayjs', selfDayjs('2022-09-08'))
-    console.log('selfDayjs 离节日', Math.ceil(selfDayjs(selfDayjs().format('YYYY') + '-09-08').diff(selfDayjs(), 'day', true)))
+    console.log('selfDayjs 离节日', selfDayjs(selfDayjs().format('YYYY') + '-09-08').diff(selfDayjs(), 'day', true))
     console.log('selfDayjs 周岁', selfDayjs().diff('2021-01-01', 'year'))
     console.log('selfDayjs 恋爱N天', Math.ceil(selfDayjs().diff(selfDayjs('2022-09-06'), 'day', true)))
     console.log('dayjs 离节日', Math.ceil(dayjs(dayjs().format('YYYY') + '-09-08').diff(dayjs(), 'day', true)))
