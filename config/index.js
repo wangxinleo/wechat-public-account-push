42cad00c6fbb41d87190797f40a9c86c619bd47f
7777wj <113433860+7777wj@users.noreply.github.com>
2022-09-13 22:06:30 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -6,40 +6,40 @@ export const config = {
   // 公众号APP_ID
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_ID: "wx71d7dcdec1a23ac8",
+  APP_ID:"wx71d7dcdec1a23ac8",
 
   // 公众号APP_SECRET
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_SECRET: "c6f62135adb9a774b76025df753c704a",
+  APP_SECRET:"c6f62135adb9a774b76025df753c704a",
 
   // 是否给文字设置多彩颜色, 和emoji不兼容
   // 如果您使用了微信测试号的模板中含有emoji表情，请填 false
-  isShowColor: true,
+  isShowColor:true,
 
   /**
    * 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔
    */
-  USERS: ["ooIhU6AGGb0Tk-qg0f9NE6HWeSSA"]
+  USERS:["ooIhU6AGGb0Tk-qg0f9NE6HWeSSA"]
     {
       // 想要发送的人的名字
-      name: "King",
+      name:"King",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "ooIhU6AGGb0Tk-qg0f9NE6HWeSSA",
+      id:"ooIhU6AGGb0Tk-qg0f9NE6HWeSSA",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "fywncgAQvku8dD7G9a2Y_v8dZx_7aFvsXblYU5fnhJ4",
+      useTemplateId:"fywncgAQvku8dD7G9a2Y_v8dZx_7aFvsXblYU5fnhJ4",
       // 所在省份
-      province: "山东",
+      province:"山东",
       // 所在城市
-      city: "青岛",
+      city:"青岛",
       // 新历生日, 仅用作获取星座运势, 格式必须
-      horoscopeDate: '04-30',
+      horoscopeDate:'04-30',
       // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
-      horoscopeDateType: '今日',
+      horoscopeDateType:'今日',
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
-      openUrl: "https://s.weibo.com/top/summary",
+      openUrl:"https://s.weibo.com/top/summary",
       // 专属节日提醒，如果你在这里填写了节日提醒，就不会执行FESTIVALS的提醒了, 和FESTIVALS的配置方法相同，可以往下查看，我这里就不重复写了
-      festivals: "type": "*生日", "name": "魏宝宝", "year": "2002", "date": "04-30",
+      festivals:"type": "*生日", "name": "魏宝宝", "year": "2002", "date": "04-30",
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {"type": "*生日", "name": "魏宝宝", "year": "2002", "date": "04-30"},
         {"type": "节日", "name": "结婚纪念日", "year": "2020", "date": "09-03"},
@@ -187,7 +187,7 @@ export const config = {
     * keyword是指暴露给测试号的模板字段，填什么就暴露什么, 请注意不要和README的出参表中的字段重复。
     * 比如：keyword: "lover_prattle" ，在测试号中就是 {{ lover_prattle.DATA }}
     * */
-  SLOT_LIST: ["keyword": "encourage_oneself", contents: "你主要的问题在于读书太少而想得太多"
+  SLOT_LIST:["keyword": "encourage_oneself", contents: "你主要的问题在于读书太少而想得太多"
     // 这样配置的话，就会每次发送这句话
     {"keyword": "encourage_oneself", contents: "你主要的问题在于读书太少而想得太多"},
     // 这样配置的话，就会每次随机选一句话发送
