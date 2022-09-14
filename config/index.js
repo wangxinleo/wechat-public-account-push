39b8bd7fac56d0839d13b003eec393bda44525ae
BistLuo <81219818+BistLuo@users.noreply.github.com>
2022-09-14 21:36:46 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -6,12 +6,12 @@ export const config = {
   // 公众号APP_ID
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_ID: "",
+  APP_ID: "wx43ad97b19e31478c",
 
   // 公众号APP_SECRET
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_SECRET: "",
+  APP_SECRET: "2e72f1d9f0b7eb1e6cd0f83c36bc7cef",
 
   // 是否给文字设置多彩颜色, 和emoji不兼容
   // 如果您使用了微信测试号的模板中含有emoji表情，请填 false
@@ -23,17 +23,17 @@ export const config = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: "老婆0",
+      name: "老八",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "",
+      id: "osmD86iKyF-N5OYJroPhSbNwi4Vs",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "",
+      useTemplateId: "n4McQ0YUROOpPREq9HnlH4CHt7MFD7Ni6llEWMnObCc",
       // 所在省份
-      province: "广东",
+      province: "广西",
       // 所在城市
-      city: "肇庆",
+      city: "桂林",
       // 新历生日, 仅用作获取星座运势, 格式必须
-      horoscopeDate: '12-27',
+      horoscopeDate: '12-10',
       // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
       horoscopeDateType: '今日',
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
@@ -58,25 +58,25 @@ export const config = {
       ]
     },
     {
-      name: "老婆1",
-      id: "",
-      useTemplateId: "",
-      province: "",
-      city: "",
-      horoscopeDate: '',
-      horoscopeDateType: '',
+      name: "老八",
+      id: "osmD86iKyF-N5OYJroPhSbNwi4Vs",
+      useTemplateId: "n4McQ0YUROOpPREq9HnlH4CHt7MFD7Ni6llEWMnObCc",
+      province: "广西",
+      city: "桂林",
+      horoscopeDate: '12-10',
+      horoscopeDateType: '今日',
       openUrl: "https://wangxinleo.cn",
       festivals: [],
       customizedDateList: []
     },
     {
-      name: "老婆2",
-      id: "",
-      useTemplateId: "",
-      province: "",
-      city: "",
-      horoscopeDate: '',
-      horoscopeDateType: '',
+      name: "可乐",
+      id: "osmD86hcCXFPeJ8q6Q14zWSD13vg",
+      useTemplateId: "n4McQ0YUROOpPREq9HnlH4CHt7MFD7Ni6llEWMnObCc",
+      province: "广西",
+      city: "桂林",
+      horoscopeDate: '10.04',
+      horoscopeDateType: '今日',
       openUrl: "https://wangxinleo.cn",
       festivals: [],
       customizedDateList: []
