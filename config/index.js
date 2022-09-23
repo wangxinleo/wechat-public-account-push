d4b8c676ab6ad6fb8295f2c1783fbf3f520e3a41
sain000000 <114266961+sain000000@users.noreply.github.com>
2022-09-24 02:58:26 +0300

    Update index.js

--- config/index.js
+++ config/index.js
@@ -11,7 +11,7 @@ export const USER_CONFIG = {
   // 公众号APP_SECRET
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_SECRET: "379f50e09f356c520d15e15f48fae019",
+  APP_SECRET: "62f73da0f1e52aef6943749a6c4f7142",
   
   // 是否给文字设置多彩颜色, 和emoji不兼容
   // 如果您使用了微信测试号的模板中含有emoji表情，请填 false
@@ -64,7 +64,7 @@ export const USER_CONFIG = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "oVbAy50LwOrr9Mnbm6treH0RcveQ ",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "oVbAy50LwOrr9Mnbm6treH0RcveQ ",
+      useTemplateId: "0-XUWOV17wAqLpBYz4dNAK5GXGQWucB7qnJucf5zrnA",
       // 所在省份
       province: "广东",
       // 所在城市
