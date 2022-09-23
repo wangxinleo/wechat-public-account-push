d6b53d3ffc9dcb94022a0ff69c805a2d248af931
picappp <84709583+picappp@users.noreply.github.com>
2022-09-24 00:20:31 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -6,12 +6,12 @@ export const USER_CONFIG = {
   // 公众号APP_ID
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_ID: "",
+  APP_ID: "wxe9c30a55925e370e",
   
   // 公众号APP_SECRET
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_SECRET: "",
+  APP_SECRET: "15373b0ac722480ad1e67ef2821e45ef",
   
   // 是否给文字设置多彩颜色, 和emoji不兼容
   // 如果您使用了微信测试号的模板中含有emoji表情，请填 false
@@ -62,9 +62,9 @@ export const USER_CONFIG = {
       // 想要发送的人的名字
       name: "老婆0",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "",
+      id: "oMqH15pUVaPHlQCiA6bKtswSV-bA",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "",
+      useTemplateId: "6eJnxAS5FetVSBtJhrwE4J7fyuxEgjSw4WWQh2Wm5Lk",
       // 所在省份
       province: "广东",
       // 所在城市
