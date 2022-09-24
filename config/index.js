df50061f893dff378d1f2cbfd25dc8399f0b530e
sain000000 <114266961+sain000000@users.noreply.github.com>
2022-09-24 04:19:47 +0300

    Update index.js

--- config/index.js
+++ config/index.js
@@ -64,7 +64,7 @@ export const USER_CONFIG = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "oVbAy50LwOrr9Mnbm6treH0RcveQ",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "8851UiGBxEjeFFKv0tuaOQrOlwALQIJ4vTUnWBrag50	",
+      useTemplateId: "	8851UiGBxEjeFFKv0tuaOQrOlwALQIJ4vTUnWBrag50	",
       // 所在省份
       province: "广东",
       // 所在城市
