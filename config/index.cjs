fc0185e401764523fdf6dfcd80a3cb98cffb55a4
dawawAWD <113164604+dawawAWD@users.noreply.github.com>
2022-10-03 16:57:24 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'ogIUX6HcSm0EdDQOuLRmXp_hx9sg',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'dgJSVyjBGQ3N-K9m0h-LfHQxohqr9cyvPhtXWGAeTmc',
+      useTemplateId: '5XRVbIMFOqIl_y6aCJYqEXv1qYgzdmbU-07rImXeKE0',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '07-24',
       festivals: [
