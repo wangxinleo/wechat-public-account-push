76dc3fac3a9cbdd0f309c9128a912058c9bf04a7
ZzX1998116 <76679392+ZzX1998116@users.noreply.github.com>
2022-10-14 23:37:06 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -67,7 +67,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oqz6O6TCw50wJJp9tPlNx7ErNyp4',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'TCC7IciOGnirehIQ9-VF0hZiEfnlh2CzkRY34B0bP1g',
+      useTemplateId: 'MitolUxx2qvxaQRYgFbi0ABMaoKzp_vUVMxdVv7T46U',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '11-06',
       festivals: [
@@ -108,8 +108,8 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
+  CALLBACK_TEMPLATE_ID: 'MitolUxx2qvxaQRYgFbi0ABMaoKzp_vUVMxdVv7T46U',
   CALLBACK_TEMPLATE_ID: 'TCC7IciOGnirehIQ9-VF0hZiEfnlh2CzkRY34B0bP1g',
-
   CALLBACK_USERS: [
     {
       name: '自己',
