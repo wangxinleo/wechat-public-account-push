07e80038e51fba58f930c6c0c470aecc7edbdf57
lyuxianzhe <117162151+lyuxianzhe@users.noreply.github.com>
2022-11-01 17:59:36 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,19 +21,19 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oLuj453LrJaUgHnmUJrHw_maFp3U',
+      id: 'oLuj456eonccNfojOdqm42Ekmk-8',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'L703iM50zGwcoPDKLTOfs32jaJFa6keLeAFxCT2iieU',
+      useTemplateId: 'mQRgcVZfY_f7A5asvfznDgQgY2P0xu4LbhnND1zJEwk',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '05-26',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
+          type: '*生日', name: '大傻逼', year: '1994', date: '06-27',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
+          type: '生日', name: '小傻逼', year: '1996', date: '05-26',
         },
         {
           type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
@@ -51,7 +51,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'L703iM50zGwcoPDKLTOfs32jaJFa6keLeAFxCT2iieU',
+  CALLBACK_TEMPLATE_ID: 'mQRgcVZfY_f7A5asvfznDgQgY2P0xu4LbhnND1zJEwk',
 
   CALLBACK_USERS: [
     {
