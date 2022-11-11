7c40b3e1e37f1d05680ca66fe3387801520df9c0
w12w221 <117983241+w12w221@users.noreply.github.com>
2022-11-11 20:14:15 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -13,8 +13,8 @@ const USER_CONFIG = {
   // 使用微信测试号：公众号APP_SECRET
   APP_SECRET: '632a5fc8b32ec1c0eb735441c674ba48',
 
-  PROVINCE: '广东',
-  CITY: '惠州',
+  PROVINCE: '山东',
+  CITY: '历城',
 
   USERS: [
     {
