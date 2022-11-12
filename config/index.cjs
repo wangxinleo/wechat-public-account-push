ad4cf143c0aa56c9bbb19840d02ae35d4d55a925
w12w221 <117983241+w12w221@users.noreply.github.com>
2022-11-12 13:37:26 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,8 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝贝老婆',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oFLTa6e3LJfvG9UyTyfzqTwqdiRA''oFLTa6U5sIp02eTyBMXlJsDfTosw',
+      id: 'oFLTa6e3LJfvG9UyTyfzqTwqdiRA',
+      id: 'oFLTa6U5sIp02eTyBMXlJsDfTosw',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '	kOBnNXrBGtv8XkaKrY-OV8KiO9aa3bLy-GpxZFryG0Q',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
