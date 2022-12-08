9ac32d304c8ca8d6c7103ced4ed8592d77f4294f
Wrappers <zhanglive@outlook.com>
2022-12-08 16:07:39 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -7,23 +7,24 @@
  */
 const USER_CONFIG = {
 
+  SE_PASSAGE: 'push-deer',
   // 使用微信测试号：公众号APP_ID
   APP_ID: '',
 
   // 使用微信测试号：公众号APP_SECRET
   APP_SECRET: '',
 
-  PROVINCE: '广东',
-  CITY: '惠州',
+  PROVINCE: '浙江',
+  CITY: '杭州',
 
   USERS: [
     {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'PDU18513TzYo0tlfTketuOqwjmrdFLNH7jdjvkm0L',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: '0001',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
