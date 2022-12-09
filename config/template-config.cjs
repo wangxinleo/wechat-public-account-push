1db84bb1fde835745096a2546869d687fe26d21b
Wrappers <zhanglive@outlook.com>
2022-12-09 09:56:45 +0800

    Update template-config.cjs

--- config/template-config.cjs
+++ config/template-config.cjs
@@ -24,7 +24,7 @@ const TEMPLATE_CONFIG = [
       
       ---
       
-      今天是我们相识的第{{love_day.DATA}}天
+      今天是我们恋爱的第{{love_day.DATA}}天
       
       {{birthday_message.DATA}}
       
@@ -32,7 +32,7 @@ const TEMPLATE_CONFIG = [
       
       {{moment_copyrighting.DATA}}
       
-      {{poetry_title.DATA}} {{poetry_content.DATA}}
+//       {{poetry_title.DATA}} {{poetry_content.DATA}}
     `
   },
   {
