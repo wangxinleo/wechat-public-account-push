508f1002bd984cbc29038068187cf5b02cef8329
1289586153 <76687071+1289586153@users.noreply.github.com>
2023-01-13 21:34:18 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -29,22 +29,15 @@ const USER_CONFIG = {
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '1997', date: '12-17',
+          type: '*生日', name: '海星', year: '1997', date: '12-17',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
+          type: '生日', name: '林七七小仙女', year: '1998', date: '09-31',
         },
         {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
+          type: '节日', name: '春节', year: '2023', date: '01-22',
         },
-      ],
-      // 我们在一起已经有xxxx天了的配置
-      customizedDateList: [
-        // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
-        // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
       ],
     },
   ],
