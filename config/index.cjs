cab58644741ac9d3af51e5f1409ac3ac32ce60ff
1289586153 <76687071+1289586153@users.noreply.github.com>
2023-01-13 22:41:22 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -51,12 +51,6 @@ const USER_CONFIG = {
       name: '海星',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oZIu45vv-UTdygfiyvvyul3GmWBs',
-      服务器信息：{{post_time_zone.DATA}} {{post_time.DATA}}
-
-共推送 {{need_post_num.DATA}} 人
-成功: {{success_post_num.DATA}} | 失败: {{fail_post_num.DATA}}
-成功用户: {{success_post_ids.DATA}}
-失败用户: {{fail_post_ids.DATA}}
     }
   ],
 
