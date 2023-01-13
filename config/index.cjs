524b0b6724954a201277340ace3cd3dcc33bfbb7
1289586153 <76687071+1289586153@users.noreply.github.com>
2023-01-13 23:09:41 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '世界上最漂亮的林七七小仙女',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oZIu45vv-UTdygfiyvvyul3GmWBs',
+      id: 'oZIu45htGZ5HPECg66amBXj3LJfQ',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'GquUw7_ioPRWMszy7pQkjq9dH7MF93M3hDdYoSJE7Zo',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
