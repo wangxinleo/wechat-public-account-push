8a3efe0b649dda3ac8da9c75d645dc8573b1bc77
Xnhyacinth <77869259+Xnhyacinth@users.noreply.github.com>
2023-01-16 18:20:15 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,20 +23,20 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o1sO_6hXhD4-ql6MzR3sA8V8OFgI',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'WFk3dVGMxlmy837j1jXjEzcbHa2xyzKKYpHSeNo4tDg',
+      useTemplateId: 'EbJjTPMtWrQfcmYUDETorMVv_QqxjA_rIYGQ3-0VCcw',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '10-31',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '2004', date: '09-18',
+          type: '*生日', name: '楠宝贝儿', year: '2004', date: '09-18',
+        },
+        {
+          type: '生日', name: '小楠的Mr.Right', year: '2001', date: '07-04',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '楠宝贝儿', year: '2004', date: '10-31',
-        },
-        {
-          type: '节日', name: '相识', year: '2022', date: '12-25',
+          type: '节日', name: '相识一年', year: '2022', date: '12-25',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
@@ -51,13 +51,13 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '	SdFMlMVzjvQPmEPGvlvtqmrYiLvb75P5fbSCxJhw2tk',
+  CALLBACK_TEMPLATE_ID: 'SdFMlMVzjvQPmEPGvlvtqmrYiLvb75P5fbSCxJhw2tk',
 
   CALLBACK_USERS: [
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'SdFMlMVzjvQPmEPGvlvtqmrYiLvb75P5fbSCxJhw2tk',
+      id: 'o1sO_6gMlAfHuUhBznoKnjtnCw_Q',
     }
   ],
 
