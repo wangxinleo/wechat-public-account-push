cbfbe3d90bd9e1248c1c38243a2c82edcd0c7e3e
1289586153 <76687071+1289586153@users.noreply.github.com>
2023-02-27 23:37:09 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -73,7 +73,7 @@ const USER_CONFIG = {
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oZIu45vv-UTdygfiyvvyul3GmWBs	',
+      id: 'oZIu45vv-UTdygfiyvvyul3GmWBs',
     }
   ],
 
