46588e53da4faa807a6dea0f84c306d6d7a049ac
liangzheyi <112047112+liangzheyi@users.noreply.github.com>
2023-03-13 10:59:01 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -27,7 +27,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oflue509txg2m4lyZ7SXx2OKdjmM',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '	TvPGRmdNdOV3DFlNCNi8Wnmed0zLWVsjGtdlioKyLNA',
+      useTemplateId: 'TvPGRmdNdOV3DFlNCNi8Wnmed0zLWVsjGtdlioKyLNA',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '10-19',
       festivals: [
