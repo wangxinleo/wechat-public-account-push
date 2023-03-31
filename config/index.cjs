ee1b14931c06370a4de61367d1a4ec179dda4304
honghuayu <36469681+javaAndyhy@users.noreply.github.com>
2023-03-31 16:33:10 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -43,7 +43,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '纯宝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'PDU21409TViZAStcjDr0CV5hJqOKCZS1ZJ00VDW6c',
+      id: 'PDU21414TAXwD6WbjHVTYGYzQhHnSUw4hqVGlTvy4',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '0001',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
