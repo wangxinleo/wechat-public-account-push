28585d30dd55663dd8030e60f8c46ab93c6f7e8e
honghuayu <36469681+javaAndyhy@users.noreply.github.com>
2023-03-31 16:23:51 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -17,6 +17,27 @@ const USER_CONFIG = {
   PROVINCE: '福建',
   CITY: '厦门',
 
+  TIAN_API: {
+    // 天行API KEY，如果使用天行API则需要填写此项
+    key: '482ceffaa26a712f48eba73d5ed19723',
+    
+    /** 天行API相关，需要config中配置 TIAN_API_KEY  */
+    // 早安心语, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    morningGreeting: true,
+  
+    // 晚安心语, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    eveningGreeting: false,
+  
+    // 天行天气（展示未来N天，最多7天）, 填 0 则不使用，按需关闭不使用的功能可以提高运行速度
+    weather: 0,
+  
+    // 全网热搜榜（展示N条，最多30条）, 填 0 则不使用，按需关闭不使用的功能可以提高运行速度
+    networkHot: 0,
+  
+    // 全网热搜榜展示类型，默认展示概要信息: ['title': 仅展示标题, 'default': 展示概要信息]
+    networkHotType: 'default',
+  },
+
   USERS: [
     {
       // 想要发送的人的名字
@@ -30,11 +51,14 @@ const USER_CONFIG = {
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '1998', date: '04-26',
+          type: '生日', name: '纯宝', year: '1998', date: '05-21',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1998', date: '05-21',
+          type: '生日', name: '华仔', year: '1992', date: '12-03',
+        },
+        {
+          type: '生日', name: '琛少', year: '2022', date: '07-25',
         },
 
       ],
@@ -50,13 +74,13 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: '0002',
 
   CALLBACK_USERS: [
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'PDU21409TViZAStcjDr0CV5hJqOKCZS1ZJ00VDW6c',
     }
   ],
 
