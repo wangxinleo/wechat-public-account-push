c8a027fd87e6bbd0abce2cda9aa72ae94d34e5a8
AnYingLieShouTuT <108744191+AnYingLieShouTuT@users.noreply.github.com>
2023-04-01 18:18:06 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oVDFF6I5TM7fzYSteroUzmPPWur0',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'GaJxtHwgHNFd3PYANjLTcxTWcSoPL6SS66AUc0N9qrc',
+      useTemplateId: 'MwgcGR6ZOBf12IZblcB-QXSGQK81xEQ3Odyx-Zdg2dM',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-14',
       festivals: [
@@ -48,7 +48,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'GaJxtHwgHNFd3PYANjLTcxTWcSoPL6SS66AUc0N9qrc',
+  CALLBACK_TEMPLATE_ID: 'MwgcGR6ZOBf12IZblcB-QXSGQK81xEQ3Odyx-Zdg2dM',
 
   CALLBACK_USERS: [
     {
