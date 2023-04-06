505d5fc91ec15de9163ac7ef40bb7e351c76e2ae
AnYingLieShouTuT <108744191+AnYingLieShouTuT@users.noreply.github.com>
2023-04-06 20:07:05 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -44,6 +44,33 @@ const USER_CONFIG = {
         { keyword: 'marry_day', date: '2022-09-09' },
       ],
     },
+    {
+      // 想要发送的人的名字
+      name: 'me',
+      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      id: 'oVDFF6I5TM7fzYSteroUzmPPWur0',
+      // 使用微信测试号：你想对他发送的模板消息的模板ID
+      useTemplateId: 'RCoQ-NVG3DxHC6GTqzDQ6sqQWBJrNKwC4bEdvDtrSpw',
+      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
+      horoscopeDate: '12-14',
+      festivals: [
+        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+        {
+          type: '*生日', name: '宝宝', year: '1999', date: '12-14',
+        },
+        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
+        {
+          type: '*生日', name: '我的', year: '1999', date: '10-06',
+        },
+      ],
+      // 我们在一起已经有xxxx天了的配置
+      customizedDateList: [
+        // 在一起的日子
+        { keyword: 'love_day', date: '2023-03-14' },
+        // 结婚纪念日
+        { keyword: 'marry_day', date: '2022-09-09' },
+      ],
+    },
   ],
 
 
@@ -54,7 +81,7 @@ const USER_CONFIG = {
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oVDFF6I5TM7fzYSteroUzmPPWur0',
+//       id: 'oVDFF6I5TM7fzYSteroUzmPPWur0',
     }
   ],
 
