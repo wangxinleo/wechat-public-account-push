d9ffdbfe7d79fa0f49b8a5fe9bdeacd3c9ab00cc
Xnhyacinth <77869259+Xnhyacinth@users.noreply.github.com>
2023-05-12 17:30:17 +0800

    Update index.js

--- src/services/index.js
+++ src/services/index.js
@@ -705,9 +705,9 @@ export const getAggregatedData = async () => {
     note: noteCh = DEFAULT_OUTPUT.noteCh,
   } = await getCIBA()
   // 获取下一休息日
-  //const holidaytts = await getHolidaytts() || DEFAULT_OUTPUT.holidaytts
-  const holidaytts = await getHolidaytts()
-  const hdaytts = DEFAULT_OUTPUT.holidaytts
+  const hs = await getHolidaytts() || DEFAULT_OUTPUT.holidaytts
+  const holidaytts = hs.split(';')[0]
+  const hdaytts = hs[1]
   // 获取每日一言
   const {
     hitokoto: oneTalk = DEFAULT_OUTPUT.oneTalk,
