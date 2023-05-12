d4cd4978f68e6d310f4e89df2e5d59fad82ce0d3
Xnhyacinth <77869259+Xnhyacinth@users.noreply.github.com>
2023-05-12 16:50:57 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -24,7 +24,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o1sO_6gMlAfHuUhBznoKnjtnCw_Q',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'Ch6lJbSQWDm6gST2O34FmW-hYYpMW6b_bFvRQJlQEAM',
+      useTemplateId: 'mXgQp6FGA2zPWZs-CXDIcSKLqTK8siE_RyFn5nRDpW0',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '03-07',
       festivals: [
@@ -55,7 +55,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o1sO_6gMlAfHuUhBznoKnjtnCw_Q',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'Ch6lJbSQWDm6gST2O34FmW-hYYpMW6b_bFvRQJlQEAM',
+      useTemplateId: 'mXgQp6FGA2zPWZs-CXDIcSKLqTK8siE_RyFn5nRDpW0',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '07-04',
       festivals: [
