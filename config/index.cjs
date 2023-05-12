0cbce842c24c28b685c80eb8ed3f31adb6b5f571
Xnhyacinth <77869259+Xnhyacinth@users.noreply.github.com>
2023-05-12 14:04:14 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o1sO_6gMlAfHuUhBznoKnjtnCw_Q',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '0V1aiiD6WCaHj64StyBCu9-RN6bWKfhJSbHjWt_wf_Q',
+      useTemplateId: '	J9_ULGCz68TXsUK4F3naRFexnQk9DjZ-WZaryEVIhtQ',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '03-07',
       festivals: [
