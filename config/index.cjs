6df3b4990b464f8d8c2f5ca973cdc72606200ab8
sunnyangazy <62740782+sunnyangazy@users.noreply.github.com>
2023-05-17 14:56:24 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'ob-Rc5us7un9YoyJiUHnGnaZGayo',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'y5u4rFFEN8yK7ax_icMr0E0L8r1kLAuG3bp8BqG2J-A',
+      useTemplateId: '_Nll6T0mCAetlyWMsV_T0hFDk6FviGH1ej_QBUjxyrQ',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '02-29',
       festivals: [
@@ -33,10 +33,10 @@ const USER_CONFIG = {
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
+          type: '生日', name: '大宝', year: '1996', date: '09-31',
         },
         {
-          type: '节日', name: '相识纪念日', year: '2020', date: '11-03',
+          type: '节日', name: '相识', year: '2020', date: '11-03',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
@@ -44,18 +44,18 @@ const USER_CONFIG = {
         // 在一起的日子
         { keyword: 'love_day', date: '2022-10-11' },
         // 结婚纪念日
-        { keyword: 'marry_day', date: '2025-09-09' },
+        { keyword: 'marry_day', date: '2022-07-29' },
       ],
     },
     {
       // 想要发送的人的名字
       name: '大宝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'ob-Rc5us7un9YoyJiUHnGnaZGayo',
+      id: 'ob-Rc5vlUii2ZWnB2yO72CSDg4Os',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'y5u4rFFEN8yK7ax_icMr0E0L8r1kLAuG3bp8BqG2J-A',
+      useTemplateId: '_Nll6T0mCAetlyWMsV_T0hFDk6FviGH1ej_QBUjxyrQ',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '02-29',
+      horoscopeDate: '03-04',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
