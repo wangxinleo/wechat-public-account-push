ef9e0e33543f6b72fba1f86c62a73adec482fa5e
sunnyangazy <62740782+sunnyangazy@users.noreply.github.com>
2023-05-17 14:20:46 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'ob-Rc5us7un9YoyJiUHnGnaZGayo',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'KeWpcUaxzrmUAFTGs4d2r2QGsLZbISxzoKo165sAGVU',
+      useTemplateId: 'jln0udhE_z94r2dS9qTC1re9R_M7QrBq0OWr_30yMMc',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '02-29',
       festivals: [
