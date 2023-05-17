29b8ff8762ac819a5e1b9cbea049019a880f002f
sunnyangazy <62740782+sunnyangazy@users.noreply.github.com>
2023-05-17 15:06:14 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -63,10 +63,10 @@ const USER_CONFIG = {
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
+          type: '生日', name: '大宝', year: '1996', date: '09-31',
         },
         {
-          type: '节日', name: '相识纪念日', year: '2020', date: '11-03',
+          type: '节日', name: '相识', year: '2020', date: '11-03',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
@@ -74,7 +74,7 @@ const USER_CONFIG = {
         // 在一起的日子
         { keyword: 'love_day', date: '2022-10-11' },
         // 结婚纪念日
-        { keyword: 'marry_day', date: '2025-09-09' },
+        { keyword: 'marry_day', date: '2022-07-29' },
       ],
     },
   ],
