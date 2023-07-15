5a8dbf9e9b88ce982531f8cb09e83cf1887e6a7f
Huanxuan Liao <liaohuanxuan2023@ia.ac.cn>
2023-07-16 00:33:27 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -15,26 +15,27 @@ const USER_CONFIG = {
 
   PROVINCE: '北京',
   CITY: '石景山',
-  TO_NAME: '人族大帝',
+  TO_NAME: '我的小庄大人',
 
   USERS: [
     {
       // 想要发送的人的名字
-      name: '人族大帝',
+      name: '我的小庄大人',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'o1sO_6lBVWOqRsUs3n2DX9XvzHIE',
+      // id: 'o1sO_6lBVWOqRsUs3n2DX9XvzHIE',
+      id: 'o1sO_6gMlAfHuUhBznoKnjtnCw_Q',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'kHsBpne3sPJCm4_ec84YjcJmPkMTT6HiHgRewh-00b4',
+      useTemplateId: 'GOTpA1LXzjt-rGJF3AuQMO_pVJYZFnIuRXYdMn5952E',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '03-07',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '生日', name: '人族大帝', year: '2003', date: '03-07',
+          type: '生日', name: '小廖的美丽小庄', year: '2003', date: '03-07',
         },
-//         {
-//           type: '生日', name: '小楠的Mr.Right', year: '2001', date: '07-04',
-//         },
+        {
+          type: '生日', name: '小庄的听话小廖', year: '2001', date: '07-04',
+        },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
 //         {
 //           type: '节日', name: '相识一年', year: '2022', date: '12-25',
@@ -43,7 +44,7 @@ const USER_CONFIG = {
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2023-01-31' },
+        { keyword: 'love_day', date: '2023-07-15' },
         // 结婚纪念日
         { keyword: 'marry_day', date: '有点久远' },
       ],
@@ -55,16 +56,16 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o1sO_6gMlAfHuUhBznoKnjtnCw_Q',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'kHsBpne3sPJCm4_ec84YjcJmPkMTT6HiHgRewh-00b4',
+      useTemplateId: 'GOTpA1LXzjt-rGJF3AuQMO_pVJYZFnIuRXYdMn5952E',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '07-04',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-//         {
-//           type: '*生日', name: '楠宝贝儿', year: '2004', date: '09-18',
-//         },
         {
-          type: '生日', name: 'Xnhyacinth', year: '2001', date: '07-04',
+          type: '生日', name: '我的美丽老婆', year: '2003', date: '03-07',
+        },
+        {
+          type: '生日', name: 'xnhyacinth', year: '2001', date: '07-04',
         },
 //         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
 //         {
@@ -74,7 +75,7 @@ const USER_CONFIG = {
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2023-01-31' },
+        { keyword: 'love_day', date: '2023-07-15' },
         // 结婚纪念日
         { keyword: 'marry_day', date: '有点久远' },
       ],
