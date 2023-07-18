0df676ff8a69d16a904d15247e114c3e0b999235
Huanxuan Liao <liaohuanxuan2023@ia.ac.cn>
2023-07-18 12:22:19 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -20,10 +20,10 @@ const USER_CONFIG = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: '小庄宝贝儿&大人&夫人&老婆&最爱的人&anyone everyone',
+      name: '宝贝儿&夫人&老婆&最爱的人&anyone everyone',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      // id: 'o1sO_6lBVWOqRsUs3n2DX9XvzHIE',
-      id: 'o1sO_6gMlAfHuUhBznoKnjtnCw_Q',
+      id: 'o1sO_6lBVWOqRsUs3n2DX9XvzHIE',
+      // id: 'o1sO_6gMlAfHuUhBznoKnjtnCw_Q',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'jRcXLp-TyD4CsVMxwf9rRzfeqFQDVmuT3HdtvkyYz4U',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
