0a7959ee50703f94be3c4687415250fd173bc262
SmileYunyao <110090916+SmileYunyao@users.noreply.github.com>
2023-07-28 21:40:36 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oEfDW5vaciY7uvm4zeUcLk_rjVnc',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '8qOSoGUEBMtHXNaMsw3NohM-1-geks7CGWqefc93nqU',
+      useTemplateId: '1BNV3lVW6ErjfJ_vFdnB3qB9XmgWnGonj266IKywtAg',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '01-25',
       festivals: [
@@ -51,7 +51,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '1fLMZc5skyGeVKuV2Oc6mAZmFVLs7mgY_FUUJP42ObI',
+  CALLBACK_TEMPLATE_ID: 'mHgKYG7Mn_dT8-rSqtUzXDqdBLcbdnbMnMPKlkq_tb8',
 
   CALLBACK_USERS: [
     {
