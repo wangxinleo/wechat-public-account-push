2a0b351885cdc47e79d8a814b809806cb6d85ffa
大鲨鱼爱吃皮皮 <46467974+MisterZongqiang@users.noreply.github.com>
2024-03-14 16:26:46 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -52,6 +52,33 @@ const USER_CONFIG = {
         //{ keyword: 'marry_day', date: '2022-09-09' },
       ],
     },
+    {
+      // 想要发送的人的名字
+      name: '主人',
+      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      id: 'PDU28835TloCEtabodZ0WqPAQefk0corbxdjWzoPv',
+      // 使用微信测试号：你想对他发送的模板消息的模板ID
+      useTemplateId: '0001',
+      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
+      horoscopeDate: '10-04',
+      festivals: [
+        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+        {
+          type: '*生日', name: '自己', year: '1998', date: '07-14',
+        },
+        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
+        {
+          type: '生日', name: '自己', year: '1998', date: '09-04',
+        },
+      ],
+      // 我们在一起已经有xxxx天了的配置
+      customizedDateList: [
+        // 在一起的日子
+        //{ keyword: 'love_day', date: '2023-06-23' },
+        // 结婚纪念日
+        //{ keyword: 'marry_day', date: '2022-09-09' },
+      ],
+    },
   ],
 
 
