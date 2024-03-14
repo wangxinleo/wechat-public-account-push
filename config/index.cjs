0615face224c575489144a8ccbd33d82b129b213
大鲨鱼爱吃皮皮 <46467974+MisterZongqiang@users.noreply.github.com>
2024-03-14 15:49:28 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oUeDV5qToUnd1jJzKB-i3GCO-cOg',
+      id: 'PDU28835TloCEtabodZ0WqPAQefk0corbxdjWzoPv',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '0001',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
@@ -59,7 +59,7 @@ const USER_CONFIG = {
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oUeDV5qToUnd1jJzKB-i3GCO-cOg',
+      id: 'PDU28835TloCEtabodZ0WqPAQefk0corbxdjWzoPv',
     }
   ],
 
