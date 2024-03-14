95f7ecacb6cb7cfe0ed176e8ee87c0fdd9a78fd9
大鲨鱼爱吃皮皮 <46467974+MisterZongqiang@users.noreply.github.com>
2024-03-14 16:32:13 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -58,7 +58,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'PDU28835TloCEtabodZ0WqPAQefk0corbxdjWzoPv',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '0001',
+      useTemplateId: '0003',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '10-04',
       festivals: [
