b8158dad00f920a01e511956a824af2056c6b939
大鲨鱼爱吃皮皮 <46467974+MisterZongqiang@users.noreply.github.com>
2024-03-14 16:30:29 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -54,7 +54,7 @@ const USER_CONFIG = {
     },
     {
       // 想要发送的人的名字
-      name: '主人',
+      name: '自己',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'PDU28835TloCEtabodZ0WqPAQefk0corbxdjWzoPv',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
