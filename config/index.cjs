2750f8c57164fbcb51e3d1d97a9f158ac18997bd
SmileYunyao <110090916+SmileYunyao@users.noreply.github.com>
2025-07-10 05:06:50 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -111,10 +111,10 @@ const USER_CONFIG = {
       name: '南松',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       // 使用其他通道时，请严格按照各个通道的教程进行填写
-      id: 'o1eBAvvVNpPwTBzHPK3NNxSNgkFE',
+      id: '	o1eBAvvVNpPwTBzHPK3NNxSNgkFE',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       // 使用其他通道时，请填写 config/template-config.cjs 中某个想要使用的模板的id
-      useTemplateId: '3Ns6PQd87WBXj4L5Ac1uGEx5z1S8js',
+      useTemplateId: '	rC0r9dZ5EQIr6TL2gmLCn2cdEv1aieNmdeZU0xCjkmE',
       // 所在省份或城市，也可以不填
       province: '广东',
       // 所在城市或县区
