e0f63989ea9dae6ae4a1bbf83353e79109c7d8f7
SmileYunyao <110090916+SmileYunyao@users.noreply.github.com>
2025-07-10 03:39:06 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -8,56 +8,56 @@
 const USER_CONFIG = {
 
   // 使用微信测试号：公众号APP_ID
-  APP_ID: 'wxf655c5eddce1afec',
+  APP_ID: 'wx6c4ab40b2b1ef720',
 
   // 使用微信测试号：公众号APP_SECRET
-  APP_SECRET: '529e24210937bc39202190c8ff0ab301',
+  APP_SECRET: '816d1ee08672e677f76b4fb47d6e6c56',
 
-  PROVINCE: '贵州',
-  CITY: '毕节',
+  PROVINCE: '广东',
+  CITY: '深圳',
 
   USERS: [
     {
       // 想要发送的人的名字
-      name: '玉儿',
+      name: '南松',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oEfDW5vaciY7uvm4zeUcLk_rjVnc',
+      id: 'o1eBAvvVNpPwTBzHPK3NNxSNgkFE',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'SCJ5YP9vyuffwlfD5AOmmwI1UekYndqeEkZqIGiB99A',
+      useTemplateId: 'Klvdf6ssZPBl-3Ns6PQd87WBXj4L5Ac1uGEx5z1S8js',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '01-25',
+      horoscopeDate: '10-03',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '玉儿', year: '1999', date: '01-25',
+          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '玉儿', year: '1999', date: '01-25',
+          type: '生日', name: '李四', year: '1996', date: '09-31',
         },
         {
-          type: '节日', name: '相识纪念日', year: '2022', date: '11-19',
+          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-11-19' },
+        { keyword: 'love_day', date: '2022-09-08' },
         // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-11-19' },
+        { keyword: 'marry_day', date: '2022-09-09' },
       ],
     },
   ],
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'mHgKYG7Mn_dT8-rSqtUzXDqdBLcbdnbMnMPKlkq_tb8',
+  CALLBACK_TEMPLATE_ID: 'UGNU2nF06QTWlxX-JchfPKb8Sw_WXOWVOK__ebY0X6GRZ4tpavUNdjq2F2k',
 
   CALLBACK_USERS: [
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oEfDW5sNy97jiSbwRYFgA5xcnyeI',
+      id: 'o1eBAvt0gvs6SQbbMz0Jr9HgdAOY',
     }
   ],
 
