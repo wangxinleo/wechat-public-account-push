1267dc7e7076b2dd951a602f4099810970d1aac4
SmileYunyao <110090916+SmileYunyao@users.noreply.github.com>
2025-11-19 13:26:19 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -114,7 +114,7 @@ const USER_CONFIG = {
       id: 'o1eBAvvVNpPwTBzHPK3NNxSNgkFE',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       // 使用其他通道时，请填写 config/template-config.cjs 中某个想要使用的模板的id
-      useTemplateId: 'i7tA6uHU3uabfKrQm6gpNJEi7mCCIdCIdvewUPvfPSA',
+      useTemplateId: 'NHgbP-CKrhRHhlTC4onj4kDkntA2shOpo4bThVx8dUA',
       // 所在省份或城市，也可以不填
       province: '贵州',
       // 所在城市或县区
