65a4fb7c9b60038c7999b8d82dfaa068c203ec94
SmileYunyao <110090916+SmileYunyao@users.noreply.github.com>
2025-11-19 13:21:09 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -165,28 +165,28 @@ const USER_CONFIG = {
         }
       }
     },
-    {
-      name: '韩老板',
-      id: 'o1eBAvjc0KgC4oiHfV4BkrOUmEAk',
-      useTemplateId: 'i7tA6uHU3uabfKrQm6gpNJEi7mCCIdCIdvewUPvfPSA',
-      province: '浙江',
-      city: '温州',
-      horoscopeDate: '09-27',
-      horoscopeDateType: '今日',
-      openUrl: 'https://wangxinleo.cn',
-      festivals: [
+    //{
+      //name: '韩老板',
+      //id: 'o1eBAvjc0KgC4oiHfV4BkrOUmEAk',
+      //useTemplateId: 'i7tA6uHU3uabfKrQm6gpNJEi7mCCIdCIdvewUPvfPSA',
+      //province: '浙江',
+      //city: '温州',
+      //horoscopeDate: '09-27',
+      //horoscopeDateType: '今日',
+      //openUrl: 'https://wangxinleo.cn',
+      //festivals: [
      //注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-        {
-          type: '*生日', name: '韩娜', year: '2000', date: '09-27',
-        },
+        //{
+          //type: '*生日', name: '韩娜', year: '2000', date: '09-27',
+        //},
     //注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
-        {
-          type: '生日', name: '韩娜', year: '2000', date: '09-27',
-        },
-      ],
-      customizedDateList: [],
-      courseSchedule: null
-     },
+        //{
+          //type: '生日', name: '韩娜', year: '2000', date: '09-27',
+        //},
+      //],
+      //customizedDateList: [],
+      //courseSchedule: null
+     //},
     // {
     //   name: '老婆2',
     //   id: '',
